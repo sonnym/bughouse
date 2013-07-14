87600c3d32d19911b0c1b11eae632212d97d06e4
sonnym <michaud.sonny@gmail.com>
2013-07-14 00:33:19 -0400

    fix client side board references

--- public/js/ib.js
+++ public/js/ib.js
@@ -65,6 +65,8 @@ var ib = (function() {
   // initial state
 
   function init(action) {
+    require.ensure(["board"], function(require) { ib.Board = require("board"); });
+
     // add ability to get keys from objects
     // http://snipplr.com/view/10430/jquery-object-keys/ => https://groups.google.com/group/jquery-en/browse_thread/thread/3d35ff16671f87a2%5C
     $.extend({ keys: function(obj) {
@@ -75,10 +77,10 @@ var ib = (function() {
             });
 
     // board is required first
-    var board = new Board();
+    var board = new ib.Board();
 
     // create and display
-    for (var b in boards) boards[b].obj = new Board();
+    for (var b in boards) boards[b].obj = new ib.Board();
 
     $("#welcome").remove();
 
