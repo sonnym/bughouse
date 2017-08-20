a47fa70b29ea51c50ad97be1e91f4ffecab6c77a
Sonny Michaud <michaud.sonny@gmail.com>
2017-08-20 18:55:39 -0400

    remove extraneous request handler

--- src/server/index.js
+++ src/server/index.js
@@ -26,19 +26,6 @@ app.use(express.static("public"), (req, res) => {
   logger.info({ req, res }, "Served static file")
 })
 
-app.get("/", (req, res) => res.send(`
-  <html>
-    <head>
-    </head>
-
-    <script src="bundle.js">
-
-    <body>
-      <h1>...</h1>
-    </body>
-  </html>
-`))
-
 app.all("*", (req, res) => logger.info({ req, res }, "Invalid request"))
 
 app.listen(port, () => logger.info(`Listening on port ${port}`))
