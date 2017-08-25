f0cc32a5b6bc59994b9400004798e757d4c0b026
Sonny Michaud <michaud.sonny@gmail.com>
2017-08-25 17:43:28 -0400

    depend on babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,17 @@ module.exports = {
     filename: "public/bundle.js",
     sourceMapFilename: "public/bundle.map"
   },
-  devtool: "#source-map"
+  devtool: "#source-map",
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }]
+  }
 }
