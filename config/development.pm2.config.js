76b5fc4bd479a897dbf47f415a649caa9d68ab64
Sonny Michaud <michaud.sonny@gmail.com>
2017-08-29 13:39:16 -0400

    liberate from nodemon and depend on pm2

---
+++ config/development.pm2.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "apps" : [{
+    "name": "express",
+    "script": "./src/server/index.js",
+    "interpreter": "./node_modules/.bin/babel-node",
+    "watch": ["src/server"],
+    "ignore_watch": ["*"],
+    "watch_options": {
+      "usePolling": true,
+      "alwaysStat": true,
+      "useFsEvents": false
+    },
+    "env": {
+        "NODE_ENV": "development"
+    }
+  }, {
+		"name": "webpack",
+		"script": "./node_modules/.bin/webpack",
+    "watch": false,
+    "args": "--progress --watch --cache"
+	}]
+}
