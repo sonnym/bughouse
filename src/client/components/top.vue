cd918eeb28e7e6df34183435cbc4f56a703c1ac0
Sonny Michaud <michaud.sonny@gmail.com>
2017-08-29 15:00:38 -0400

    whitespace

--- src/client/components/top.vue
+++ src/client/components/top.vue
@@ -14,14 +14,15 @@
      <a class="awesome blue large" onclick="bughouse.next()">&rarr;</a>
      <a class="awesome blue large" onclick="bughouse.tail()">&#x21ac;</a>
     </div>
+
     <div id="play" class="hidden">
-    <label>
-      Show Moves
-      <input type="checkbox" onchange="bughouse.toggleShowMoves(this.checked);" checked>
-    </label>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a class="awesome blue" onclick="bughouse.toggleFlipBoard();">Flip Boards</a>
-    <a class="awesome blue" onclick="bughouse.redraw_boards();">Redraw Boards</a>
+      <label>
+        Show Moves
+        <input type="checkbox" onchange="bughouse.toggleShowMoves(this.checked);" checked>
+      </label>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a class="awesome blue" onclick="bughouse.toggleFlipBoard();">Flip Boards</a>
+      <a class="awesome blue" onclick="bughouse.redraw_boards();">Redraw Boards</a>
     </div>
   </div>
 </template>
