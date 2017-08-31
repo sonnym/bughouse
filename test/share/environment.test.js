c2629a87ca6afb678bb7483104b855573861dba9
Sonny Michaud <michaud.sonny@gmail.com>
2017-08-31 18:55:36 -0400

    remove borken tests

--- test/share/environment.test.js
+++
@@ -1,7 +0,0 @@
-import test from "ava"
-import { environment } from "./../../src/share/environment"
-
-test("environment is development by default", t => {
-  t.is(environment, "development")
-  t.pass()
-})
