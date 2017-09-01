43922959542a37dea135baa72e6bc62dc2188ebd
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-01 19:43:14 -0400

    fix component with multiple root elements

--- src/client/components/controls.vue
+++ src/client/components/controls.vue
@@ -10,26 +10,28 @@
 </script>
 
 <template>
-  <div id="kibitz">
-    <a class="awesome blue large" onclick="bughouse.head()">&#x21ab;</a>
-    <a class="awesome blue large" onclick="bughouse.prev()">&larr;</a>
+  <section id="controls">
+    <div id="kibitz">
+      <a class="awesome blue large" onclick="bughouse.head()">&#x21ab;</a>
+      <a class="awesome blue large" onclick="bughouse.prev()">&larr;</a>
 
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-    <a class="awesome blue large" onclick="bughouse.next()">&rarr;</a>
-    <a class="awesome blue large" onclick="bughouse.tail()">&#x21ac;</a>
-  </div>
+      <a class="awesome blue large" onclick="bughouse.next()">&rarr;</a>
+      <a class="awesome blue large" onclick="bughouse.tail()">&#x21ac;</a>
+    </div>
 
-  <div id="play" v-if="playing">
-    <label>
-      Show Moves
-      <input type="checkbox" onchange="bughouse.toggleShowMoves(this.checked);" checked>
-    </label>
+    <div id="play" v-if="playing">
+      <label>
+        Show Moves
+        <input type="checkbox" onchange="bughouse.toggleShowMoves(this.checked);" checked>
+      </label>
 
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-    <a class="awesome blue" onclick="bughouse.toggleFlipBoard();">Flip Boards</a>
-    <a class="awesome blue" onclick="bughouse.redraw_boards();">Redraw Boards</a>
-  </div>
+      <a class="awesome blue" onclick="bughouse.toggleFlipBoard();">Flip Boards</a>
+      <a class="awesome blue" onclick="bughouse.redraw_boards();">Redraw Boards</a>
+    </div>
+  </section
 </template>
