c5572e9b7701b1d894d0f73e115694cc85f86190
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-01 18:18:10 -0400

    whitespace

--- src/client/components/top.vue
+++ src/client/components/top.vue
@@ -10,12 +10,14 @@
 <template>
   <header>
     <div id="kibitz">
-     <a class="awesome blue large" onclick="bughouse.head()">&#x21ab;</a>
-     <a class="awesome blue large" onclick="bughouse.prev()">&larr;</a>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-     <a class="awesome blue large" onclick="bughouse.next()">&rarr;</a>
-     <a class="awesome blue large" onclick="bughouse.tail()">&#x21ac;</a>
+      <a class="awesome blue large" onclick="bughouse.head()">&#x21ab;</a>
+      <a class="awesome blue large" onclick="bughouse.prev()">&larr;</a>
+
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+      <a class="awesome blue large" onclick="bughouse.next()">&rarr;</a>
+      <a class="awesome blue large" onclick="bughouse.tail()">&#x21ac;</a>
     </div>
 
     <div id="play" v-if="playing">
@@ -23,7 +25,9 @@
         Show Moves
         <input type="checkbox" onchange="bughouse.toggleShowMoves(this.checked);" checked>
       </label>
+
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
       <a class="awesome blue" onclick="bughouse.toggleFlipBoard();">Flip Boards</a>
       <a class="awesome blue" onclick="bughouse.redraw_boards();">Redraw Boards</a>
     </div>
