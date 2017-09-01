67adbe1a62f72c5b2a06f2ddcb4aaf3c0131aa7c
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-01 19:47:37 -0400

    stub out initial navigation elements

--- src/client/components/navigation.vue
+++ src/client/components/navigation.vue
@@ -1,11 +1,25 @@
 <script>
   export default {
-    name: "navigation"
+    name: "navigation",
+    data: function() {
+      return {
+        loggedIn: false
+      }
+    }
   }
 </script>
 
 <template>
   <nav>
     <h1>∞</h1>
+
+    <ul v-if="!loggedIn">
+      <li><a href="">Log In</a></li>
+    </ul>
+
+    <ul v-if="loggedIn">
+      <li><a href="">Profile</a></li>
+      <li><a href="">Log Out</a></li>
+    </ul>
   </nav>
 </template>
