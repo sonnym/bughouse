b00c0c439716ef8aede29a30bf20cf7733fd1f5d
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-01 18:38:01 -0400

    factor out controls component

--- src/client/components/top.vue
+++ src/client/components/top.vue
@@ -1,13 +1,12 @@
 <script>
   import Navigation from "./navigation"
+  import Controls from "./controls"
 
   export default {
     name: "top",
-    data: {
-      playing: false
-    },
     components: {
-      navigation: Navigation
+      navigation: Navigation,
+      controls: Controls
     }
   }
 </script>
@@ -15,28 +14,6 @@
 <template>
   <header>
     <navigation></navigation>
-
-    <div id="kibitz">
-      <a class="awesome blue large" onclick="bughouse.head()">&#x21ab;</a>
-      <a class="awesome blue large" onclick="bughouse.prev()">&larr;</a>
-
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-      <a class="awesome blue large" onclick="bughouse.next()">&rarr;</a>
-      <a class="awesome blue large" onclick="bughouse.tail()">&#x21ac;</a>
-    </div>
-
-    <div id="play" v-if="playing">
-      <label>
-        Show Moves
-        <input type="checkbox" onchange="bughouse.toggleShowMoves(this.checked);" checked>
-      </label>
-
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-      <a class="awesome blue" onclick="bughouse.toggleFlipBoard();">Flip Boards</a>
-      <a class="awesome blue" onclick="bughouse.redraw_boards();">Redraw Boards</a>
-    </div>
+    <controls></controls>
   </header>
 </template>
