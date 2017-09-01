03bb535d27943901ce650a8f854635269763cff3
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-01 17:58:25 -0400

    initial model testing

--- test/server/model.test.js
+++ test/server/model.test.js
@@ -1,3 +1,11 @@
 import test from "ava"
+import Model from "./../../src/server/model"
 
-test.failing(() => { })
+test.beforeEach(t => {
+  t.context = new Model()
+})
+
+test("attributes after initialization", t => {
+  t.is(t.context.clients.length, 0)
+  t.is(t.context.waiting.length, 0)
+})
