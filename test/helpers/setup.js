48b218bf4ce876264b4f3d4f5538a905cf772c5d
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-06 03:30:48 -0400

    remove unhelpful comments

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -11,11 +11,6 @@ process.on("uncaughtException", (err) => {
 
 browserEnv()
 
-// Setup Vue.js to remove production tip
 Vue.config.productionTip = false;
-
-// Setup vue files to be processed by `require-extension-hooks-vue`
 hooks("vue").plugin("vue").push()
-
-// Setup vue and js files to be processed by `require-extension-hooks-babel`
 hooks(["vue", "js"]).plugin("babel").push()
