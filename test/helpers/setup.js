989770ae16a553ad1df055e902dbc985dde0e340
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-06 03:45:32 -0400

    override database for every test
    using the pid to maintain isolation in ava

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -4,6 +4,8 @@ import browserEnv from "browser-env"
 import hooks from "require-extension-hooks"
 import Vue from "vue"
 
+import { inject, connect } from "./../../src/server/database"
+
 process.on("uncaughtException", (err) => {
   console.log("EXCEPTION:")
   console.log(inspect(err))
@@ -14,3 +16,11 @@ browserEnv()
 Vue.config.productionTip = false;
 hooks("vue").plugin("vue").push()
 hooks(["vue", "js"]).plugin("babel").push()
+
+inject({
+  database: `bughouse_test_${process.pid}`,
+  dialect: "sqlite",
+  storage: ":memory:"
+})
+
+connect()
