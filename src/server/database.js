234ac60d3021a0220d4e61b0ad0ed865d6779cb6
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-06 03:57:20 -0400

    log sql rather than printing to console

--- src/server/database.js
+++ src/server/database.js
@@ -1,14 +1,21 @@
 import Sequelize from "sequelize"
 
 import config from "./../../config/sequelize.config"
-import { environment } from "./../share/environment"
 
-let orm = new Sequelize(config[environment])
+import loggerServer from "./logger"
+import { environment, isDevelopment, isTest } from "./../share/environment"
+
+const logger = loggerServer()
+const commonConfig = {
+  benchmark: isDevelopment() || isTest(),
+  logging: (msg, ms) => logger.info(`Executed SQL (${ms} ms): ${msg}`)
+}
+
+let orm = new Sequelize(Object.assign({}, commonConfig, config[environment]))
 
 const connect = () => orm.authenticate()
 const inject = (config) => {
-  const original = orm
-  orm = new Sequelize(config)
+  orm = new Sequelize(Object.assign({}, commonConfig, config))
 }
 
 export { orm, connect, inject }
