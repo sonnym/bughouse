4fa1550bb042f8e0a6aa9deedcc7bc93c8568d27
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-06 06:15:17 -0400

    ability to verify user passwords

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,7 +1,28 @@
+import bcrypt from "bcrypt"
+
 import model from "./index"
 
-export default model((define, {UUID}) => {
-  return define("User", {
-    uuid: UUID
+const saltRounds = 8
+
+const User = model((define, {UUID, UUIDV4, STRING}) => {
+  return define("users", {
+    uuid: {
+      type: UUID,
+      defaultValue: UUIDV4
+    },
+
+    passwordHash: STRING
   })
 })
+
+User.hook("beforeSave", async (user, options) => {
+  if (user.password && user.password.length > 0) {
+    user.passwordHash = await bcrypt.hash(user.password, saltRounds)
+  }
+})
+
+User.prototype.isValidPassword = async function(plaintext) {
+  return await bcrypt.compare(plaintext, this.passwordHash)
+}
+
+export default User
--- test/app/models/user.test.js
+++ test/app/models/user.test.js
@@ -13,3 +13,32 @@ test("building instance", t => {
 test("uuid property", t => {
   t.not(User.build().uuid, null)
 })
+
+test("automatically hashes password before save", async t => {
+  const password = "foobarbaz"
+  const user = User.build()
+
+  t.is(user.password, undefined)
+  t.is(user.passwordHash, undefined)
+
+  user.password = password
+
+  await user.save()
+
+  t.true(await user.isValidPassword(password))
+  t.false(await user.isValidPassword("fizzbuzz"))
+})
+
+test("does not attempt to hash empty", async t => {
+  const user = User.build({ password: null })
+  await user.save()
+
+  t.is(user.passwordHash, undefined)
+})
+
+test("does not attempt to hash zero length passwords", async t => {
+  const user = User.build({ password: null })
+  await user.save()
+
+  t.is(user.passwordHash, undefined)
+})
