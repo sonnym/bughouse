1574ef63b4e6b08c1345d776ff866c1af5e69b35
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-08 15:23:22 -0400

    test mounting of components

--- test/client/components/navigation.vue.test.js
+++ test/client/components/navigation.vue.test.js
@@ -1,8 +1,21 @@
 import test from "ava"
-import "./../../helpers/component"
 
+import Vue, { initRouter } from "./../../helpers/component"
 import Navigation from "./../../../src/client/components/navigation"
+
+test.beforeEach("initialize vue router", t => {
+  const router = initRouter()
+
+  t.context.vm = new Vue({
+    router,
+    render: (h) => h(Navigation)
+  }).$mount()
+})
 
 test("Navigation is an object", t => {
   t.true(Navigation instanceof Object)
+})
+
+test("Navigation mounted", t => {
+  t.truthy(t.context.vm.$el)
 })
--- test/client/components/top.vue.test.js
+++ test/client/components/top.vue.test.js
@@ -1,8 +1,21 @@
 import test from "ava"
-import "./../../helpers/component"
 
+import Vue, { initRouter } from "./../../helpers/component"
 import Top from "./../../../src/client/components/top"
+
+test.beforeEach("initialize vue router", t => {
+  const router = initRouter()
+
+  t.context.vm = new Vue({
+    router,
+    render: (h) => h(Top)
+  }).$mount()
+})
 
 test("Top is an object", t => {
   t.true(Top instanceof Object)
+})
+
+test("Top mounted", t => {
+  t.truthy(t.context.vm.$el)
 })
