8f298b758838ed396c8a8e1aa6328c05411eaa79
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-08 15:11:09 -0400

    ability to initialize vue router in tests

--- test/helpers/component.js
+++ test/helpers/component.js
@@ -1,7 +1,15 @@
 import "./browser"
 
 import Vue from "vue"
+import VueRouter from "vue-router"
+
+import routes from "./../../src/client/routes"
 
 Vue.config.productionTip = false
 
 export default Vue
+export const initRouter = () => {
+  Vue.use(VueRouter)
+
+  return new VueRouter({ routes })
+}
