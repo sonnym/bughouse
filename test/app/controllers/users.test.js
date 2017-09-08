29736541e3d04a5c2c74ce722d2a9af583f1b234
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-08 05:25:23 -0400

    users controller

---
+++ test/app/controllers/users.test.js
@@ -0,0 +1,41 @@
+import test from "ava"
+import { stub, mock } from "sinon"
+
+import User from "./../../../src/app/models/user"
+import * as UsersController from "./../../../src/app/controllers/users"
+
+test.beforeEach("set up request and response", t => {
+  t.context.req = { user: User.build() }
+  t.context.res = { json: () => {} }
+
+  mock(t.context.res).expects("json").once
+})
+
+test.afterEach("verify mock", t => {
+  mock.verify()
+})
+
+test("index", async t => {
+  await UsersController.index(t.context.req, t.context.res)
+  t.pass()
+})
+
+test("create", t => {
+  UsersController.create(t.context.req, t.context.res)
+  t.pass()
+})
+
+test("show", t => {
+  UsersController.show(t.context.req, t.context.res)
+  t.pass()
+})
+
+test("update", t => {
+  UsersController.update(t.context.req, t.context.res)
+  t.pass()
+})
+
+test("destroy", t => {
+  UsersController.destroy(t.context.req, t.context.res)
+  t.pass()
+})
