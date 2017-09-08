8c1ef6dd1a5f7ef321cf6e01733ec4ce66a1193f
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-08 00:56:02 -0400

    remove injection logic
    since SQLite in-memory databases operate are isolated by process, they
    already work perfectly with ava when tests in a given file are run
    serially

--- test/helpers/database.js
+++ test/helpers/database.js
@@ -4,42 +4,17 @@ import Sequelize from "sequelize"
 import Umzug from "umzug"
 
 import loggerServer from "./../../src/server/logger"
-import { inject, connect } from "./../../src/server/database"
+import { orm, connect } from "./../../src/server/database"
 
 const logger = loggerServer()
 
-const databaseName = `bughouse_test_${process.pid}`
-logger.info(`Creating database: ${databaseName}`)
-
-const orm = inject({
-  database: databaseName,
-  dialect: "sqlite",
-  storage: ":memory:",
-  logging: (msg, ms) => logger.info(`Executed SQL on (${databaseName}) (${ms} ms): ${msg}`),
-  retry: {
-    max: 0
-  }
-})
-
-const umzug = new Umzug({
-  storage: "sequelize",
-  logging: (msg) => logger.info(`Migration on (${databaseName}): ${msg}`),
-  migrations: {
-    path: join(process.cwd(), "db", "migrations"),
-    params: [orm.getQueryInterface(), Sequelize]
-  },
-  storageOptions: {
-    sequelize: orm,
-  }
-})
-
-const connectAndMigrate = async () => {
+const connectAndSync = async () => {
   await connect()
-  await umzug.up()
+  await orm.sync()
 }
 
 export default test => {
-  test.before("set up database connnection", async t => await connectAndMigrate())
+  test.before("set up database connnection", async t => await connectAndSync())
   test.beforeEach("truncate all tables", async t => await orm.truncate())
   test.after.always("close database connection", t => orm.close())
 }
