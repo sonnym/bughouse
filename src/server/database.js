dacfd99cf979dea582b681098f9fbfc8f1e1268b
Sonny Michaud <michaud.sonny@gmail.com>
2017-09-12 07:00:32 -0400

    replace sequelize with bookshelf in server

--- src/server/database.js
+++ src/server/database.js
@@ -1,22 +1,18 @@
-import Sequelize from "sequelize"
+import config from "./../../config/knex.config"
 
-import config from "./../../config/sequelize.config"
+import knex from "knex"
+import bookshelf from "bookshelf"
 
 import loggerServer from "./logger"
 import { environment, isDevelopment, isTest } from "./../share/environment"
 
 const logger = loggerServer()
 const commonConfig = {
-  benchmark: isDevelopment() || isTest(),
+  debug: isDevelopment() || isTest(),
   logging: (msg, ms) => logger.info(`Executed SQL (${ms} ms): ${msg}`),
-  define: {
-    timestamps: true,
-    paranoid: true
-  }
 }
 
-let orm = new Sequelize(Object.assign({}, commonConfig, config[environment]))
+const connection = knex(Object.assign({}, commonConfig, config[environment]))
+const orm = bookshelf(connection)
 
-const connect = () => orm.authenticate()
-
-export { orm, connect }
+export { orm, connection }
