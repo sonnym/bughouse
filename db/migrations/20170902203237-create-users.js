96ed93d3dcc254562e06ad3dc06b44d2dff490e7
Sonny Michaud <michaud.sonny@gmail.com>
2017-12-28 18:54:21 -0500

    default value for uuid

--- db/migrations/20170902203237-create-users.js
+++ db/migrations/20170902203237-create-users.js
@@ -4,7 +4,11 @@ module.exports = {
       table.increments()
       table.timestamps()
 
-      table.uuid("uuid").notNullable().unique()
+      table.uuid("uuid")
+        .notNullable()
+        .unique()
+        .defaultTo(knex.raw('uuid_generate_v4()'))
+
       table.index("uuid")
 
       table.string("password_hash").nullable()
