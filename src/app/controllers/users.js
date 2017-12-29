83c7ce31a5758dd9aa53170327a8b941dc15352c
Sonny Michaud <michaud.sonny@gmail.com>
2017-12-28 21:48:29 -0500

    basic user creation controller action

--- src/app/controllers/users.js
+++ src/app/controllers/users.js
@@ -1,7 +1,20 @@
 import User from "./../models/user"
 
 export const index = async (req, res) => res.json(await User.fetchAll())
-export const create = (req, res) => res.json({ })
+
+export const create = async (req, res) => {
+  let user = User.forge(req.body)
+
+  if (await user.save()) {
+    res.user = user
+    res.location("/")
+  } else {
+    res.location("/")
+  }
+
+  res.end()
+}
+
 export const show = (req, res) => res.json(req.user)
 export const update = (req, res) => res.json({ })
 export const destroy = (req, res) => res.json(req.user.destroy())
--- test/app/controllers/users.test.js
+++ test/app/controllers/users.test.js
@@ -5,41 +5,65 @@ import User from "./../../../src/app/models/user"
 import * as UsersController from "./../../../src/app/controllers/users"
 
 test.beforeEach("set up request and response", t => {
-  t.context.req = { user: User.forge() }
-  t.context.res = { json: () => {} }
-
-  t.context.mock = mock(t.context.res).expects("json").once()
+  t.context.req = { user: new User }
+  t.context.res = {
+    json: () => {},
+    location: () => {},
+    end: () => {}
+  }
 })
 
 test.afterEach("verify mock", t => {
-  t.context.mock.verify()
 })
 
 test("index", async t => {
+  let jsonMock = mock(t.context.res).expects("json").once()
   let fetchMock = mock(User).expects("fetchAll").once().returns([User.forge()])
 
   await UsersController.index(t.context.req, t.context.res)
 
   fetchMock.verify()
+  jsonMock.verify()
+
   t.pass()
 })
 
-test("create", t => {
-  UsersController.create(t.context.req, t.context.res)
+test("create", async t => {
+  let locationMock = mock(t.context.res).expects("location").once()
+  let endMock = mock(t.context.res).expects("end").once()
+  let userMock  = mock(User).expects("forge").once().returns({ save: () => true })
+
+  await UsersController.create(t.context.req, t.context.res)
+
+  locationMock.verify()
+  endMock.verify()
+
   t.pass()
 })
 
 test("show", t => {
+  let jsonMock = mock(t.context.res).expects("json").once()
+
   UsersController.show(t.context.req, t.context.res)
+  jsonMock.verify()
+
   t.pass()
 })
 
 test("update", t => {
+  let jsonMock = mock(t.context.res).expects("json").once()
+
   UsersController.update(t.context.req, t.context.res)
+  jsonMock.verify()
+
   t.pass()
 })
 
 test("destroy", t => {
+  let jsonMock = mock(t.context.res).expects("json").once()
+
   UsersController.destroy(t.context.req, t.context.res)
+  jsonMock.verify()
+
   t.pass()
 })
