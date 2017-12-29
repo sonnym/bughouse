f2541ec5365b1c523c3e2b3da73bb2c946c7177f
Sonny Michaud <michaud.sonny@gmail.com>
2017-12-28 20:23:54 -0500

    basic signup form

---
+++ src/client/components/signup.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    name: "signup"
+  }
+</script>
+
+<template>
+  <content>
+    <h2>Sign Up</h2>
+
+    <section>
+      <form action="/users" method="post">
+        <input type="email" placeholder="email" required>
+        <br>
+        <input type="password" placeholder="password" required>
+        <br>
+        <button>Submit</button>
+      </form>
+    </section>
+  </content>
+</template>
--- src/client/routes.js
+++ src/client/routes.js
@@ -1,9 +1,11 @@
 import Games from "./components/games"
 import Login from "./components/login"
+import Signup from "./components/signup"
 
 const routes = [
   { path: "/", component: Games },
   { path: "/login", component: Login },
+  { path: "/signup", component: Signup },
 
   { path: "*", redirect: { to: "/" } }
 ]
