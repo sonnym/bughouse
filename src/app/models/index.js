593d3fdf710cd4dd3e7f03311d50e7a1b5fa9a0e
Sonny Michaud <michaud.sonny@gmail.com>
2017-12-30 14:13:20 -0500

    bind transaction method to orm object

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -1,6 +1,6 @@
 import { orm } from "./../../server/database"
 
-export const transaction = orm.transaction
+export const transaction = orm.transaction.bind(orm)
 
 export default orm.Model
 export const __useDefault = true
