289d1f30f37bcd9ca4fdcb8e6b38e4f8d7b28438
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-02 23:50:42 -0600

    basic login form

--- src/client/components/login.vue
+++ src/client/components/login.vue
@@ -8,6 +8,16 @@
   <content>
     <h2>Login</h2>
 
+    <section>
+      <form action="/sessions" method="post" enctype="application/x-www-url-formencoded">
+        <input name="email" type="email" placeholder="email" required>
+        <br>
+        <input name="password" type="password" placeholder="password" required>
+        <br>
+        <button>Submit</button>
+      </form>
+    </section>
+
     <p>
       Not registered?
       <router-link to="/signup">Sign up!</router-link>
