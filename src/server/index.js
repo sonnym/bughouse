e5b00a64b25a973bccbbac56e4b2bf35698801bc
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-02 22:12:45 -0600

    include content length in log

--- src/server/index.js
+++ src/server/index.js
@@ -25,11 +25,12 @@ const app = express()
 export const logger = loggerServer()
 export const socketHook = SocketHandler => socketServer(app, SocketHandler)
 export const routerHook = RouterHandler => RouterHandler(app, express.Router)
+export const authenticationHook = AuthenticationHandler => AuthenticationHandler(passport)
 
 export function startServer(port = 3000) {
   app.use((req, res, next) => {
     res.on("finish", () => {
-      logger.info({ req, res }, `[${req.method}] (${req.ip}) ${req.path} ${res.statusCode}`)
+      logger.info({ req, res }, `[${req.method}] (${req.ip}) ${req.path} ${res.statusCode} ${res.get('Content-Length')}`)
     })
 
     next()
