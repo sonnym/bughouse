c3287be5fa57d07f97b0e0fc3613301ccb628eff
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-04 18:33:48 -0600

    use pid to determine port
    to prevent collisions

--- test/integration/authentication.test.js
+++ test/integration/authentication.test.js
@@ -3,15 +3,19 @@ import request from "supertest"
 
 import { v4 } from "uuid"
 
-import app from "./../helpers/app"
+import appGenerator from "./../helpers/app"
 import User from "./../../src/app/models/user"
+
+test.beforeEach(t => {
+  t.context.app = appGenerator()
+})
 
 test("user can request index page", async t => {
   const email = `test.${v4()}@example.com`
   const password = "test1234"
 
   await User.createWithPassword({ email, password })
-  const res = await request(app).post("/sessions").send(`email=${email}&password=${password}`)
+  const res = await request(t.context.app).post("/sessions").send(`email=${email}&password=${password}`)
 
   t.is(res.status, 302)
 })
