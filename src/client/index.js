72e1133865a3e0efc38980e4a536cfa2c6e2f525
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-05 13:26:51 -0600

    configure vuex

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 import Vue from "vue"
+import Vuex from "vuex"
 import VueRouter from "vue-router"
 import Vuetify from "vuetify"
 
@@ -7,11 +8,15 @@ import routes from "./routes"
 
 import App from "./components/app"
 
+Vue.use(Vuex)
 Vue.use(VueRouter)
 Vue.use(Vuetify)
 
+const store = new Vuex.Store({})
+
 new Vue({
   el: '#app',
+  store,
   router: new VueRouter({ routes }),
   render: (h) => h(App)
 })
