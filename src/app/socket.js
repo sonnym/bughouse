f93df374774a46a3dd07cb68f671d28afefe9f4b
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-07 16:29:44 -0600

    move bulk of socket control into app

--- src/app/socket.js
+++ src/app/socket.js
@@ -1,1 +1,35 @@
-export default class SocketHandler { }
+import Client from "./models/client"
+
+import { logger } from "./index"
+
+export default (req, ws) => {
+  const client = new Client(ws)
+
+  logger.info({ ws, user: req.user }, `Websocket connect (${client.id}) ${req.user.get("uuid")}`)
+
+  ws.on("message", (message) => {
+    logger.info({ ws, message }, `Websocket message by (${client.id}): ${message}`)
+
+    /*
+    try {
+      (({action, ...rest}) => {
+        if (!controller[action]) {
+          logger.info(`Received invalid action for dispatch: ${action}`)
+        } else {
+          logger.info({ controller, args: rest }, `Dispatching ${action}`)
+
+          controller[action](rest)
+        }
+      })(JSON.parse(message))
+    } catch(err) {
+      if (err instanceof SyntaxError) {
+        logger.info("Received invalid JSON command")
+      } else {
+        throw err
+      }
+    }
+    */
+  })
+}
+
+export const __useDefault = true
--- src/server/socket.js
+++ src/server/socket.js
@@ -1,56 +1,6 @@
-import { inspect } from "util"
-
-import { v4 } from "uuid"
 import ExpressWS from "express-ws"
 
-import getLogger from "./logger"
-
-const logger = getLogger()
-
-export default function(app, SocketController) {
-  const wss = ExpressWS(app).getWss()
-
-  app.ws("/ws", (ws, req) => {
-    const client = mkClient(ws)
-
-    logger.info({ ws }, `Websocket connection established: ${client.id}`)
-
-    const controller = new SocketController(client)
-
-    ws.on("message", (message) => {
-      logger.info({ ws, message }, `Websocket message by (${client.id}): ${message}`)
-
-      try {
-        (({action, ...rest}) => {
-          if (!controller[action]) {
-            logger.info(`Received invalid action for dispatch: ${action}`)
-          } else {
-            logger.info({ controller, args: rest }, `Dispatching ${action}`)
-
-            controller[action](rest)
-          }
-        })(JSON.parse(message))
-
-      } catch(err) {
-        if (err instanceof SyntaxError) {
-          logger.info("Received invalid JSON command")
-        } else {
-          throw err
-        }
-      }
-    })
-  })
+export default function(app, SocketHandler) {
+  ExpressWS(app).getWss()
+  app.ws("/ws", SocketHandler)
 }
-
-function mkClient(ws) {
-  return {
-    id: v4(),
-    send: (command) => {
-      logger.info({ ws, command }, `Sending command to (${this.id}): ${inspect(command)}`)
-
-      ws.send(JSON.stringify(command))
-    }
-  }
-}
-
-export const __useDefault = true
