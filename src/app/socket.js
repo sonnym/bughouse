3c0645f3402c62e9c412d5e6c4db55adf726699f
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-11 21:26:42 -0600

    remove old dispatch code

--- src/app/socket.js
+++ src/app/socket.js
@@ -29,26 +29,6 @@ export default async (ws, req) => {
 
   ws.on("message", (message) => {
     logger.info({ ws, message }, `Websocket [RECV] (${client.uuid}) ${message}`)
-
-    /*
-    try {
-      (({action, ...rest}) => {
-        if (!controller[action]) {
-          logger.info(`Received invalid action for dispatch: ${action}`)
-        } else {
-          logger.info({ controller, args: rest }, `Dispatching ${action}`)
-
-          controller[action](rest)
-        }
-      })(JSON.parse(message))
-    } catch(err) {
-      if (err instanceof SyntaxError) {
-        logger.info("Received invalid JSON command")
-      } else {
-        throw err
-      }
-    }
-    */
   })
 }
 
