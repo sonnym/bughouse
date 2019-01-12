1c10b0d1a77ec9d0cf8454c6e29de5b5a3fb7af2
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-11 22:02:27 -0600

    constantize active user key

--- src/app/models/universe.js
+++ src/app/models/universe.js
@@ -2,6 +2,8 @@ import { promisify } from "util"
 
 import redis from "redis"
 
+const ACTIVE_USERS = "activeUsers"
+
 export default class Universe {
   static get redisClient() {
     if (!this._redisClient) {
@@ -12,19 +14,19 @@ export default class Universe {
   }
 
   static addUser() {
-    this.redisClient.incr("activeUsers")
+    this.redisClient.incr(ACTIVE_USERS)
   }
 
   static removeUser() {
-    this.redisClient.decr("activeUsers")
+    this.redisClient.decr(ACTIVE_USERS)
   }
 
   static async init() {
-    return await promisify(this.redisClient.set).bind(this.redisClient)("activeUsers", 0)
+    return await promisify(this.redisClient.set).bind(this.redisClient)(ACTIVE_USERS, 0)
   }
 
   static async activeUsers() {
-    return await promisify(this.redisClient.get).bind(this.redisClient)("activeUsers")
+    return await promisify(this.redisClient.get).bind(this.redisClient)(ACTIVE_USERS)
   }
 
   static async serialize() {
