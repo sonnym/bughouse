50659dab803d69913c2ae96da6e45c9b3d59928d
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-11 21:54:56 -0600

    move client methods onto client

--- src/app/models/client.js
+++ src/app/models/client.js
@@ -1,17 +1,40 @@
+import redis from "redis"
 import { v4 } from "uuid"
 
+import Universe from "./universe"
+
 import { logger } from "./../index"
+
+const redisClient = redis.createClient({ db: 1 })
 
 export default class Client {
   constructor(socket, user) {
     this.socket = socket
     this.user = user
 
-    this._uuid = v4()
+    this.uuid = v4()
   }
 
-  get uuid() {
-    return this._uuid
+  async connected() {
+    logger.info(this.logData, `Websocket [OPEN] (${this.uuid}) ${this.userUuid}`)
+
+    redisClient.incr("activeUsers")
+
+    this.send({
+      action: "connected",
+      data: {
+        universe: await Universe.serialize()
+      }
+    })
+  }
+
+  close() {
+    logger.info(this.logData, `Websocket [CLOSE] (${this.uuid}) ${this.userUuid}`)
+    redisClient.decr("activeUsers")
+  }
+
+  message(message) {
+    logger.info(this.logData, `Websocket [RECV] (${this.uuid}) ${message}`)
   }
 
   send(command) {
@@ -20,5 +43,13 @@ export default class Client {
     logger.info({ socket: this.socket, command }, `Websocket [SEND] (${this.uuid}) ${json}`)
 
     this.socket.send(json)
+  }
+
+  get userUuid() {
+    return this.user ? this.user.get("uuid") : 'unknown'
+  }
+
+  get logData() {
+    return { socket: this.socket, user: this.user }
   }
 }
