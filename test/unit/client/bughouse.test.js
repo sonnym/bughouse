bcef7964cfeb07da75d2d67ce2704dcbfcfa58d5
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-11 18:41:48 -0600

    pass app into bughouse call

--- test/unit/client/bughouse.test.js
+++ test/unit/client/bughouse.test.js
@@ -1,4 +1,5 @@
 import test from "ava"
+import { stub } from "sinon"
 
 import WebSocket from "ws"
 
@@ -7,6 +8,7 @@ import bughouse from "./../../../src/client/bughouse"
 test.beforeEach(t => global.WebSocket = WebSocket)
 
 test("function call", t => {
-  bughouse()
+  const app = { $store: { commit: stub() } }
+  bughouse.bind(app).call()
   t.pass()
 })
