c27f4ce3a9f4ec05f246825b86d0f703645bbcfb
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-11 18:43:46 -0600

    pass store into socket initializer

--- test/unit/client/socket.test.js
+++ test/unit/client/socket.test.js
@@ -1,12 +1,13 @@
 import test from "ava"
 
 import WebSocket from "ws"
+import { initStore } from "./../../helpers/component"
 
 import Socket from "./../../../src/client/socket"
 
 test.beforeEach(t => global.WebSocket = WebSocket)
 
 test("constructor", t => {
-  new Socket()
+  new Socket(initStore())
   t.pass()
 })
