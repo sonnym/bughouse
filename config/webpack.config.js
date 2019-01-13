18eb7042db5565da6effc0fb3d26f4a714406cd2
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-13 17:04:37 -0600

    configure css loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -26,6 +26,12 @@ module.exports = {
       test: /\.vue$/,
       loader: "vue-loader",
     }, {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader"
+      ]
+    }, {
       test: /\.scss$/,
       use: [
         MiniCssExtractPlugin.loader,
