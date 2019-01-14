490dcd4ce3b8a0491383f965d7ce69de9f272ed9
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-13 18:34:00 -0600

    use id for session serialization

--- src/app/authentication.js
+++ src/app/authentication.js
@@ -3,10 +3,10 @@ import { logger } from "./../server/index"
 import User from "./models/user"
 
 export default (passport) => {
-  passport.serializeUser((user, done) => done(null, user.get("uuid")))
+  passport.serializeUser((user, done) => { done(null, user.get("id")) })
 
-  passport.deserializeUser(async (uuid, done) => {
-    logger.debug(`Negotiating session for ${uuid}`)
-    done(null, await User.where({ uuid }).fetch())
+  passport.deserializeUser(async (id, done) => {
+    logger.debug(`Negotiating session for ${id}`)
+    done(null, await User.where({ id }).fetch())
   })
 }
