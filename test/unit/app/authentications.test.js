5504bd0c324a2d2de3cfc0689e11ff84dea7be56
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-14 21:26:50 -0600

    fix authorization tests

--- test/unit/app/authentications.test.js
+++ test/unit/app/authentications.test.js
@@ -1,8 +1,6 @@
 import test from "ava"
 import sinon from "sinon"
 
-import v4 from "uuid"
-
 import authentication from "./../../../src/app/authentication"
 import User from "./../../../src/app/models/user"
 
@@ -12,25 +10,25 @@ test.before(async t => {
     deserializeUser: function(fn) { this.deserializeUser = fn }
   }
 
-  t.context.uuid = v4()
-  t.context.user = await new User({ uuid: t.context.uuid }).save()
+  t.context.user = await new User().save()
+  t.context.id = t.context.user.get("id")
 
   t.context.done = sinon.fake()
 
   authentication(t.context.passport)
 })
 
-test("serializing user returns uuid", t => {
+test("serializing user returns id", t => {
   t.context.passport.serializeUser.call(null, t.context.user, t.context.done)
 
   t.is(
     t.context.done.lastCall.lastArg,
-    t.context.uuid
+    t.context.id
   )
 })
 
 test("deserializing user returns user", async t => {
-  await t.context.passport.deserializeUser.call(null, t.context.uuid, t.context.done)
+  await t.context.passport.deserializeUser.call(null, t.context.id, t.context.done)
 
   t.is(
     t.context.done.lastCall.lastArg.get("id"),
