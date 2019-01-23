070fa49f467982fa3e80a0a96a23e2f2f9efec24
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-22 20:05:07 -0600

    remove database helper

--- test/helpers/database.js
+++
@@ -1,7 +0,0 @@
-import { connection } from "./../../src/server/database"
-
-export default test => {
-  test.before("migrate database", async t => await connection.migrate.latest())
-}
-
-export const __useDefault = true
