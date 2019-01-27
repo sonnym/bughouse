1db4e1ac84145d665402ff497d0e8628b0a5f3d7
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-27 02:05:34 -0600

    log errors to stderr

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,9 +1,11 @@
+import { stdout, stderr} from "process"
+
 import { join } from "path"
 import { inspect } from "util"
 
 import { Writable } from "stream"
 
-import { createLogger } from "bunyan"
+import { createLogger, WARN } from "bunyan"
 import { environment, isDevelopment } from "./../share/environment"
 
 const logPath = join(process.cwd(), "log", environment)
@@ -42,9 +44,10 @@ function createStreams() {
     stream: new Writable({
       objectMode: true,
       write: (obj, _, cb) => {
+        const output = obj.level < WARN ? stdout : stderr
         const message = obj.msg.replace(/\n/g, "").replace(/\s+/g, " ")
 
-        process.stdout.write(`${obj.time.toISOString()}: ${message}\n`)
+        output.write(`${obj.time.toISOString()}: ${message}\n`)
 
         cb()
       }
