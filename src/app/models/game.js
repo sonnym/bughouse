6cf5e097c70e2f01e44b233776d5af2e82910f44
Sonny Michaud <michaud.sonny@gmail.com>
2019-01-27 18:28:49 -0600

    remove placeholder methods

--- src/app/models/game.js
+++ src/app/models/game.js
@@ -11,9 +11,6 @@ import Revision from "./revision"
 export default class Game extends Model {
   constructor(...args) {
     super(...args)
-
-    this.on("created", this.publish)
-    this.on("updated", this.unpublish)
   }
 
   static get redis() {
@@ -99,8 +96,4 @@ export default class Game extends Model {
       currentPosition: currentPosition.serialize()
     }
   }
-
-  publish() { }
-
-  unpublish() { }
 }
