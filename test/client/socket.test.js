f5f297ec50a033175a2c03423380b7f0fd0d901c
Sonny Michaud <michaud.sonny@gmail.com>
2019-02-02 17:37:32 -0600

    unpend client socket test

--- test/client/socket.test.js
+++ test/client/socket.test.js
@@ -3,13 +3,11 @@ import test from "ava"
 import WebSocket from "ws"
 import { initStore } from "@/component"
 
-/*
 import Socket from "~/client/socket"
-*/
 
 test.beforeEach(t => global.WebSocket = WebSocket)
 
-test.skip("constructor", t => {
+test("constructor", t => {
   new Socket(initStore())
   t.pass()
 })
