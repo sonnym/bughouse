1e4586222a1c2840dda966a8453e765dec7527aa
Sonny Michaud <michaud.sonny@gmail.com>
2019-02-02 16:22:13 -0600

    load user and display display name on profile

--- src/client/components/profile.vue
+++ src/client/components/profile.vue
@@ -1,2 +1,27 @@
 <template>
+  <v-container fixed fluid>
+    <div class="headline">{{ user.displayName }}</div>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        user: { }
+      }
+    },
+
+    beforeRouteEnter({ params }, _from, next) {
+      fetch(`/users/${params.uuid}`)
+        .then(response => response.json())
+        .then(json => next(vm => vm.setUser(json)))
+    },
+
+    methods: {
+      setUser(user) {
+        this.user = user
+      }
+    },
+  }
+</script>
