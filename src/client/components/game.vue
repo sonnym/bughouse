72c8d9bd9bf06e8c99442112e9db5eacfac3d6fd
Sonny Michaud <michaud.sonny@gmail.com>
2019-02-02 14:56:57 -0600

    initial player component

--- src/client/components/game.vue
+++ src/client/components/game.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="game">
+    <player v-bind:user="topPlayer"></player>
+
     <board
       v-bind:position="position"
       v-bind:inverted="inverted"
     ></board>
+
+    <player v-bind:user="bottomPlayer"></player>
   </div>
 </template>
 
 <script>
   import Board from "./board"
+  import Player from "./player"
 
   export default {
     props: ["game", "inverted"],
@@ -16,11 +21,28 @@
     computed: {
       position() {
         return this.game ? this.game.currentPosition.fen : ""
+      },
+
+      topPlayer() {
+        if (!this.game) {
+          return { }
+        }
+
+        return this.inverted ? this.game.whiteUser : this.game.blackUser
+      },
+
+      bottomPlayer() {
+        if (!this.game) {
+          return { }
+        }
+
+        return this.inverted ? this.game.blackUser : this.game.whiteUser
       }
     },
 
     components: {
-      board: Board
+      board: Board,
+      player: Player
     }
   }
 </script>
---
+++ src/client/components/player.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-chip disabled>
+    {{ user.displayName }}
+  </v-chip>
+</template>
+
+<script>
+  export default {
+    props: ["user"],
+  }
+</script>
