3bb452a9cad3f09752242eed7c37ad72cfa9c52a
Sonny Michaud <michaud.sonny@gmail.com>
2019-02-02 20:01:04 -0600

    resolve all promises in array

--- src/app/controllers/games.js
+++ src/app/controllers/games.js
@@ -3,8 +3,9 @@ import Game from "~/app/models/game"
 export const index = async ({ params }, res, next) => {
   try {
     const games = await Game.forUser(params.userUUID)
+    const gamesData = await Promise.all(games.map(game => game.serialize()))
 
-    res.status(200).send(await games.serialize())
+    res.status(200).send(gamesData)
   } catch(err) {
     res.status(404).end()
     next(err)
