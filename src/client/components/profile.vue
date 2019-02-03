ea95d357cf186150c5b9d2f527860922aaf434af
Sonny Michaud <michaud.sonny@gmail.com>
2019-02-02 20:09:32 -0600

    load and display list of games on profile

--- src/client/components/profile.vue
+++ src/client/components/profile.vue
@@ -1,14 +1,63 @@
 <template>
   <v-container fixed fluid>
-    <div class="headline">{{ user.displayName }}</div>
+    <v-flex tag="h1" class="headline">{{ user.displayName }}</v-flex>
+
+    <v-spacer></v-spacer>
+    <v-card>
+      <v-card-title>
+        Games
+      </v-card-title>
+
+      <v-spacer></v-spacer>
+
+      <v-data-table
+        :headers="headers"
+        :items="games"
+        :loading="loading"
+        hide-actions
+      >
+        <template slot="items" slot-scope="props">
+          <td>
+            <router-link v-bind:to="{ name: 'user', params: { uuid: props.item.opponent.uuid } }">
+              {{ props.item.opponent.displayName }}
+            </router-link>
+          </td>
+        </template>
+      </v-data-table>
+    </v-card>
   </v-container>
 </template>
 
 <script>
+  import { map } from "ramda"
+
   export default {
     data() {
       return {
-        user: { }
+        loading: true,
+
+        user: { },
+        gamesData: [ ],
+
+        headers: [
+          { text: "Opponent", sortable: false },
+          { text: "Result", sortable: false }
+        ]
+      }
+    },
+
+    computed: {
+      games() {
+        if (!this.gamesData) {
+          return [ ]
+        }
+
+        return map(gameDatum => {
+          return {
+            opponent: gameDatum.whiteUser.uuid === this.user.uuid ? gameDatum.blackUser : gameDatum.whiteUser,
+            result: "-"
+          }
+        }, this.gamesData)
       }
     },
 
@@ -18,10 +67,27 @@
         .then(json => next(vm => vm.setUser(json)))
     },
 
+    created() {
+      this.fetchGames()
+    },
+
     methods: {
       setUser(user) {
         this.user = user
-      }
+      },
+
+      fetchGames() {
+        fetch(`/users/${this.$route.params.uuid}/games`)
+          .then(response => response.json())
+          .then(json => {
+            this.gamesData = json
+            this.loading = false
+          })
+      },
     },
+
+    watch: {
+      "$route": "fetchGames"
+    }
   }
 </script>
