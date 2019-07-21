f49124779256e31a434a732ca6e9c6868bd196cd
Sonny Michaud <michaud.sonny@gmail.com>
2019-07-21 15:47:15 -0500

    scope controller

--- src/app/route.js
+++ src/app/route.js
@@ -1,13 +1,13 @@
 import * as UsersController from "./controllers/users"
 import * as SessionsController from "./controllers/sessions"
 
-import * as GamesController from "./controllers/games"
+import * as UsersGamesController from "./controllers/users_games"
 
 export default (app, Router) => {
   app.use("/users", resources(UsersController, new Router()))
   app.use("/sessions", resource(SessionsController, new Router()))
 
-  app.use("/users/:userUUID/games", resources(GamesController, new Router({ mergeParams: true })))
+  app.use("/users/:userUUID/games", resources(UsersGamesController, new Router({ mergeParams: true })))
 }
 
 const resources = (Controller, router) => {
--- test/app/controllers/users_games.test.js
+++ test/app/controllers/users_games.test.js
@@ -2,12 +2,12 @@ import test from "ava"
 
 import Factory from "@/factory"
 
-import * as GamesController from "~/app/controllers/games"
+import * as UsersGamesController from "~/app/controllers/users_games"
 
 test.serial("unsuccessful index", async t => {
   const user = await Factory.user()
 
-  await GamesController.index(
+  await UsersGamesController.index(
     Factory.req({ userUUID: user.get("uuid") }),
     Factory.res(t, 404),
     Factory.next(t)
@@ -20,7 +20,7 @@ test.serial("successful index", async t => {
 
   await user.refresh()
 
-  await GamesController.index(
+  await UsersGamesController.index(
     Factory.req({ userUUID: user.get("uuid") }),
     Factory.res(t, 200),
     Factory.next(t)
