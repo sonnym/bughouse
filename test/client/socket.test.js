b3d6cbf3b9ab4b4d1133008d0facebadbeeeaa28
Sonny Michaud <michaud.sonny@gmail.com>
2019-10-19 19:32:15 -0500

    remove broken override

--- test/client/socket.test.js
+++ test/client/socket.test.js
@@ -1,11 +1,8 @@
 import test from "ava"
 
-import WebSocket from "ws"
 import { initStore } from "@/component"
 
 import Socket from "~/client/socket"
-
-test.beforeEach(t => global.WebSocket = WebSocket)
 
 test("constructor", t => {
   new Socket(initStore())
