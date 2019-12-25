bd4db1dcbc47d499ec7b75d6673f99c95f2cd038
Sonny Michaud <michaud.sonny@gmail.com>
2019-12-25 09:42:33 -0600

    improve controls

--- src/client/components/controls.vue
+++ src/client/components/controls.vue
@@ -1,11 +1,13 @@
 <template>
   <section class="controls">
     <div>
-      <v-btn outlined>&#x21ab;</v-btn>
-      <v-btn @click="rotateLeft" outlined>&larr;</v-btn>
+      <v-btn @click="rotateLeft" outlined>
+        <v-icon>mdi-arrow-left-thick</v-icon>
+      </v-btn>
 
-      <v-btn @click="rotateRight" outlined>&rarr;</v-btn>
-      <v-btn outlined>&#x21ac;</v-btn>
+      <v-btn @click="rotateRight" outlined>
+        <v-icon>mdi-arrow-right-thick</v-icon>
+      </v-btn>
     </div>
   </section>
 </template>
