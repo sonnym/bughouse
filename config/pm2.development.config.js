e134be362b66fb9d16719a16f8f3b7e390f2c813
Sonny Michaud <michaud.sonny@gmail.com>
2019-12-24 22:04:47 -0600

    combine start and simulate

--- config/pm2.development.config.js
+++ config/pm2.development.config.js
@@ -20,5 +20,19 @@ module.exports = {
     "env": {
       "NODE_ENV": "development"
     }
+  }, {
+    "name": "simulation",
+    "script": "./src/simulation.js",
+    "interpreter": "./node_modules/.bin/babel-node",
+    "watch": ["src/simulation.js"],
+    "ignore_watch": ["*"],
+    "watch_options": {
+      "usePolling": true,
+      "alwaysStat": true,
+      "useFsEvents": false
+    },
+    "env": {
+      "NODE_ENV": "development"
+    }
   }]
 }
