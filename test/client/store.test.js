9ed91025fc5f5cc5b9809673551fa3eeef11f891
Sonny Michaud <michaud.sonny@gmail.com>
2019-12-25 22:03:39 -0600

    use alias for better import

--- test/client/store.test.js
+++ test/client/store.test.js
@@ -2,7 +2,7 @@ import test from "ava"
 
 import { identity } from "ramda"
 
-import store from "./../../src/client/store"
+import store from "~/client/store"
 
 test("hideNavigation", t => {
   const state = { showNavigation: true }
