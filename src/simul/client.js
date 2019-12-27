9b2bbd1dd24f1b54bbc2ea7408a1a7e00e7ee307
Sonny Michaud <michaud.sonny@gmail.com>
2019-12-26 19:34:21 -0600

    factor out player

--- src/simul/client.js
+++ src/simul/client.js
@@ -1,19 +1,17 @@
-import { pick } from "ramda"
-
 import WebSocket from "ws"
-import { Chess } from "chess.js"
 
 import makeLogger from "~/share/logger"
-import { REVISION_TYPES } from "~/share/constants"
 
 import User from "./user"
+import Player from "./player"
 
 const logger = makeLogger("simul")
 
 export default class Client {
   async run() {
-    const { cookie } = await User.create()
+    this.player = new Player(this.send.bind(this))
 
+    const { cookie } = await User.create()
     this.connect(cookie)
   }
 
@@ -32,81 +30,21 @@ export default class Client {
 
   message(message) {
     const { action, ...rest } = JSON.parse(message)
+    logger.info(`[WebSocket RECV] ${message}`)
 
-    try {
-      logger.info(`[WebSocket RECV] ${message}`)
-      this[action].call(this, rest)
-    } catch(e) {
-      logger.error(e)
-    }
+    this.player[action].call(this.player, rest)
+  }
+
+  send(command) {
+    const message = JSON.stringify(command)
+    logger.info(`[WebSocket SEND] ${message}`)
+
+    this.socket.send(message)
   }
 
   close() {
     if (this.socket) {
       this.socket.close()
     }
-  }
-
-  send(command) {
-    const message = JSON.stringify(command)
-
-    this.socket.send(message)
-    logger.info(`[WebSocket SEND] ${message}`)
-  }
-
-  universe() { }
-  wait() { }
-  games() { }
-
-  user({ user }) {
-    this.user = user
-    this.send({ action: "play" })
-  }
-
-  start({ game, opponent }) {
-    this.game = game
-    this.chess = new Chess()
-
-    if (game.whiteUser.uuid === this.user.uuid) {
-      this.color = this.chess.WHITE
-    } else if (game.blackUser.uuid === this.user.uuid) {
-      this.color = this.chess.BLACK
-    }
-
-    if (this.color === this.chess.turn()) {
-      setTimeout(this.move.bind(this), 15000)
-    }
-  }
-
-  position({ game, position }) {
-    if (this.game.uuid !== game.uuid) {
-      return
-    }
-
-    this.chess.load(position.fen)
-
-    if (this.color === this.chess.turn()) {
-      this.move()
-    }
-  }
-
-  move() {
-    const moves = this.chess.moves({ verbose: true })
-    const move = moves[Math.floor(Math.random() * moves.length)]
-
-    if (move.piece === this.chess.PAWN && move.to.match(/1|8/)) {
-      move.promotion = [
-        this.chess.KNIGHT,
-        this.chess.BISHOP,
-        this.chess.ROOK,
-        this.chess.QUEEN
-      ][Math.floor(Math.random() * 4)]
-    }
-
-    process.nextTick(this.send.bind(this, {
-      action: "revision",
-      type: REVISION_TYPES.MOVE,
-      ...pick(["from", "to", "promotion"], move)
-    }))
   }
 }
---
+++ test/simul/player.test.js
@@ -0,0 +1,61 @@
+import test from "ava"
+
+import { mock, spy } from "sinon"
+
+import Player from "~/simul/player"
+
+test("noop functions", t => {
+  const player = new Player()
+
+  t.is(typeof player.wait, "function")
+  t.is(typeof player.games, "function")
+  t.is(typeof player.universe, "function")
+})
+
+test("starts playing after receiving user information", t => {
+  const sender = spy()
+  const user = mock()
+
+  const player = new Player(sender)
+
+  player.user({ user })
+
+  t.is(user, player.user)
+  t.true(sender.called)
+})
+
+test("receives information about a game", t => {
+  const user = { uuid: 0 }
+  const game = {
+    uuid: 0,
+    whiteUser: { uuid: 0 },
+    blackUser: { uuid: 1 }
+  }
+
+  t.log("test")
+  t.log(user)
+  const player = new Player(() => {})
+
+  player.user({ user })
+  player.start({ game })
+
+  t.is(game, player.game)
+})
+
+test("receives information about positions", t => {
+  const user = { uuid: 0 }
+  const game = {
+    uuid: 0,
+    whiteUser: { uuid: 0 },
+    blackUser: { uuid: 1 }
+  }
+  const position = { fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" }
+
+  const player = new Player(() => {})
+
+  player.user({ user })
+  player.start({ game })
+  player.position({ game, position })
+
+  t.pass()
+})
