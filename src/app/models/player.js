91e7918c41e46e2547b6a1fbe776f8a507512482
Sonny Michaud <michaud.sonny@gmail.com>
2019-12-27 19:49:21 -0600

    organization

--- src/app/models/player.js
+++ src/app/models/player.js
@@ -12,19 +12,19 @@ export default class Player {
     Universe.play(this)
   }
 
-  async revision(data) {
-    const game = await Game.where({ uuid: this.serializedGame.uuid }).fetch()
-
-    if (await Revision.create(game, data)) {
-      Game.emit("revision", game)
-    }
-  }
-
   async startGame(serializedGame) {
     this.serializedGame = serializedGame
 
     await this.client.redis.subscribeAsync(serializedGame.uuid)
 
     this.client.send({ action: "start", game: serializedGame })
+  }
+
+  async revision(data) {
+    const game = await Game.where({ uuid: this.serializedGame.uuid }).fetch()
+
+    if (await Revision.create(game, data)) {
+      Game.emit("revision", game)
+    }
   }
 }
