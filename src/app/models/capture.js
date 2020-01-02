a78813b63ce0b24b24948e8c2645824ff0879f8c
Sonny Michaud <michaud.sonny@gmail.com>
2020-01-01 23:22:00 -0600

    model for handling captures

---
+++ src/app/models/capture.js
@@ -0,0 +1,63 @@
+import { CAPTURE as type } from "~/share/constants/revision_types"
+
+export default class Capture {
+  constructor(universe, Revision) {
+    this.universe = universe
+    this.games = universe.games
+
+    this.Revision = Revision
+  }
+
+  async process(source, piece) {
+    const sourceUUID = source.get("uuid")
+    const index = await this.findIndex(sourceUUID)
+
+    let targetUUID
+
+    if (toNext(index, piece)) {
+      targetUUID = await this.universe.nextGame(sourceUUID)
+    } else if (toPrev(index, piece)) {
+      targetUUID = await this.universe.prevGame(sourceUUID)
+    }
+
+    const revision = await this.Revision.create({ type, source, targetUUID, piece })
+    const position = revision.related("position")
+
+    return { uuid: targetUUID, position }
+  }
+
+  async findIndex(uuid) {
+    let index = 0, node = await this.games.head()
+
+    while (node && node !== uuid) {
+      node = await this.games.next(node)
+      index++
+    }
+
+    return index
+  }
+}
+
+function isEven(n) {
+  return n % 2 === 0
+}
+
+function isOdd(n) {
+  return n % 2 !== 0
+}
+
+function isWhite(piece) {
+  return /[PRNBQK]/.test(piece)
+}
+
+function isBlack(piece) {
+  return /[prnbqk]/.test(piece)
+}
+
+function toNext(index, piece) {
+  return (isWhite(piece) && isEven(index)) || (isBlack(piece) && isOdd(index))
+}
+
+function toPrev(index, piece) {
+  return (isWhite(piece) && isOdd(index)) || (isBlack(piece) && isEven(index) )
+}
---
+++ test/app/models/capture.test.js
@@ -0,0 +1,126 @@
+import test from "ava"
+
+import { stub } from "sinon"
+import { v4 } from "uuid"
+
+import { CAPTURE } from "~/share/constants/revision_types"
+
+import Universe from "~/app/models/universe"
+
+import Capture from "~/app/models/capture"
+
+const games = (() => {
+  const _arr = [v4(), v4(), v4()]
+
+  return {
+    head: () => _arr[0],
+    tail: () => _arr[2],
+    next: (n) => {
+      const i = _arr.indexOf(n) + 1
+      return i < _arr.length ? _arr[i] : null
+    },
+    prev: (n) => {
+      const i = _arr.indexOf(n) - 1
+      return i > -1 ? _arr[i] : null
+    }
+  }
+})()
+
+const universe = new Universe()
+universe.games = games
+
+test("process: white piece in even index goes to next", async t => {
+  const position_ = v4()
+  const create = stub().returns({
+    related: stub().returns(position_)
+  })
+  const Revision = { create }
+
+  const piece = "P"
+  const game = { get: () => { return games.tail() } }
+  const next = games.head()
+
+  const { uuid, position } = await new Capture(universe, Revision).process(game, piece)
+
+  t.is(next, uuid)
+  t.is(position_, position)
+
+  t.true(create.calledOnceWith({
+    type: CAPTURE,
+    targetUUID: next,
+    source: game,
+    piece
+  }))
+})
+
+test("process: white piece in odd index goes to prev", async t => {
+  const position_ = v4()
+  const create = stub().returns({
+    related: stub().returns(position_)
+  })
+  const Revision = { create }
+
+  const piece = "P"
+  const game = { get: () => { return games.next(games.head()) } }
+  const prev = games.head()
+
+  const { uuid, position } = await new Capture(universe, Revision).process(game, piece)
+
+  t.is(prev, uuid)
+  t.is(position_, position)
+
+  t.true(create.calledOnceWith({
+    type: CAPTURE,
+    targetUUID: prev,
+    source: game,
+    piece
+  }))
+})
+
+test("process: black piece in even index goes to prev", async t => {
+  const position_ = v4()
+  const create = stub().returns({
+    related: stub().returns(position_)
+  })
+  const Revision = { create }
+
+  const piece = "p"
+  const game = { get: () => { return games.head() } }
+  const prev = games.tail()
+
+  const { uuid, position } = await new Capture(universe, Revision).process(game, piece)
+
+  t.is(prev, uuid)
+  t.is(position_, position)
+
+  t.true(create.calledOnceWith({
+    type: CAPTURE,
+    targetUUID: prev,
+    source: game,
+    piece
+  }))
+})
+
+test("process: black piece in odd index goes to next", async t => {
+  const position_ = v4()
+  const create = stub().returns({
+    related: stub().returns(position_)
+  })
+  const Revision = { create }
+
+  const piece = "p"
+  const game = { get: () => { return games.next(games.head()) } }
+  const next = games.tail()
+
+  const { uuid, position } = await new Capture(universe, Revision).process(game, piece)
+
+  t.is(next, uuid)
+  t.is(position_, position)
+
+  t.true(create.calledOnceWith({
+    type: CAPTURE,
+    targetUUID: next,
+    source: game,
+    piece
+  }))
+})
