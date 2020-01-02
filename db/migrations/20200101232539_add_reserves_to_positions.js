782d1228450a1628848bb52b5cefa05c378d4a39
Sonny Michaud <michaud.sonny@gmail.com>
2020-01-02 00:50:59 -0600

    update migration

--- db/migrations/20200101232539_add_reserves_to_positions.js
+++ db/migrations/20200101232539_add_reserves_to_positions.js
@@ -4,12 +4,12 @@ module.exports = {
       table
         .jsonb("white_reserve")
         .notNullable()
-        .defaultTo({ })
+        .defaultTo({ P: 0, R: 0, N: 0, B: 0, Q: 0 })
 
       table
         .jsonb("black_reserve")
         .notNullable()
-        .defaultTo({ })
+        .defaultTo({ p: 0, r: 0, n: 0, b: 0, q: 0 })
 
       table.dropColumn("reserve")
     })
