aefed97b9d64768449a2808dffaa0ff3fa68f42c
Sonny Michaud <michaud.sonny@gmail.com>
2020-01-01 18:05:31 -0600

    improve user controller tests

--- src/app/controllers/users.js
+++ src/app/controllers/users.js
@@ -2,12 +2,12 @@ import User from "~/app/models/user"
 
 export const create = async (req, res, next) => {
   try {
-    const user = await User.create(req.body || { })
+    const user = await User.create(req.body)
     await user.refresh()
 
     req.login(user, async (err) => {
       if (err) next(err)
-      res.status(201).send(await user.serialize())
+      res.status(201).send(user.serialize())
     })
   } catch(err) {
     res.status(400).end()
@@ -17,9 +17,11 @@ export const create = async (req, res, next) => {
 
 export const show = async ({ params }, res, next) => {
   try {
-    const user = await new User({ uuid: params.uuid }).fetch()
+    const user = await User
+      .forge({ uuid: params.uuid })
+      .fetch()
 
-    res.status(200).send(await user.serialize())
+    res.status(200).send(user.serialize())
   } catch(err) {
     res.status(404).end()
     next(err)
