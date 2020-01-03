0aeded391e52c5e31c3fc91e43d2d8d1e80f6665
Sonny Michaud <michaud.sonny@gmail.com>
2020-01-02 18:53:03 -0600

    list consistency

--- src/app/models/redis.js
+++ src/app/models/redis.js
@@ -12,18 +12,16 @@ export default class Redis {
     this.redis = redis.createClient({ db: REDIS_DB })
   }
 
-  get flushdb() { return this.redis.flushdb.bind(this.redis) }
+  get set() { return promisify(this.redis.set).bind(this.redis) }
+  get get() { return promisify(this.redis.get).bind(this.redis) }
 
-  get multi() { return this.redis.multi.bind(this.redis) }
+  get hget() { return promisify(this.redis.hget).bind(this.redis) }
+  get hgetall() { return promisify(this.redis.hgetall).bind(this.redis) }
+
+  get flushdb() { return this.redis.flushdb.bind(this.redis) }
   get end() { return this.redis.end.bind(this.redis) }
 
-  get set() { return this.redis.set.bind(this.redis) }
-  get setAsync() { return promisify(this.redis.set).bind(this.redis) }
-
-  get getAsync() { return promisify(this.redis.get).bind(this.redis) }
-
-  get hgetAsync() { return promisify(this.redis.hget).bind(this.redis) }
-  get hgetallAsync() { return promisify(this.redis.hgetall).bind(this.redis) }
+  get multi() { return this.redis.multi.bind(this.redis) }
 
   get incr() { return this.redis.incr.bind(this.redis) }
   get decr() { return this.redis.decr.bind(this.redis) }
