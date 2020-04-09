0bd0338ab6acf43d9fea27025bba869ad97a37b9
Sonny Michaud <michaud.sonny@gmail.com>
2020-04-08 21:25:44 -0500

    stub leaderboard component

---
+++ test/client/components/ViewLeaderboard.vue.test.js
@@ -0,0 +1,14 @@
+import test from "ava"
+
+import { mount, initRouter } from "@/component"
+import ViewLeaderboard from "~/client/components/ViewLeaderboard"
+
+test("ViewLeaderboard snapshot", t => {
+  const router = initRouter()
+
+  const wrapper = mount(ViewLeaderboard, {
+    router
+  })
+
+  t.snapshot(wrapper.html())
+})
