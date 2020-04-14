5bfbe799c0a1c238bdc681e4f814c685d9b4703c
Sonny Michaud <michaud.sonny@gmail.com>
2020-04-14 02:08:25 -0500

    graphql client proof of concept

--- src/client/components/ViewHistory.vue
+++ src/client/components/ViewHistory.vue
@@ -1,9 +1,34 @@
 <template>
-  <div />
+  <div>{{ message }}</div>
 </template>
 
 <script>
+  import { gql } from "@apollo/client"
+
   export default {
-    name: "ViewHistory"
+    name: "ViewHistory",
+
+    data() {
+      return {
+        message: ""
+      }
+    },
+
+    created() {
+      this.fetchMessage()
+    },
+
+    methods: {
+      fetchMessage() {
+        const query = gql`
+          {
+            hello
+          }
+        `
+
+        this.$store.state.query({ query })
+          .then(json => this.message = json.data)
+      }
+    }
   }
 </script>
