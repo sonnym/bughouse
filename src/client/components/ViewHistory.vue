53f29466fa8ece42c14bdd6c2ec26e628965f291
Sonny Michaud <michaud.sonny@gmail.com>
2020-04-14 19:53:04 -0500

    scaffold history view

--- src/client/components/ViewHistory.vue
+++ src/client/components/ViewHistory.vue
@@ -1,16 +1,55 @@
 <template>
-  <div>{{ message }}</div>
+  <v-card
+    class="mx-2 mt-5 px-3"
+    tile
+  >
+    <v-card-title>
+      {{ message }}
+    </v-card-title>
+
+    <v-card-text>
+      <v-container>
+        <v-row>
+          <v-col>
+            <chess-game />
+          </v-col>
+
+          <v-col>
+            <v-data-table
+              :headers="headers"
+              :items="moves"
+              hide-default-footer
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
   import { gql } from "@apollo/client"
 
+  import ChessGame from "./ChessGame"
+
   export default {
     name: "ViewHistory",
 
+    components: {
+      ChessGame
+    },
+
     data() {
       return {
-        message: ""
+        message: "",
+
+        moves: [ ],
+
+        headers: [
+          { text: "#", sortable: false },
+          { text: "White", sortable: false },
+          { text: "Black", sortable: false }
+        ]
       }
     },
 
