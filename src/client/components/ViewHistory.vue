25d971b440d3ae5f3d37b5f81ec64dd0a5336476
Sonny Michaud <michaud.sonny@gmail.com>
2020-04-15 19:54:39 -0500

    initial history display

--- src/client/components/ViewHistory.vue
+++ src/client/components/ViewHistory.vue
@@ -4,22 +4,33 @@
     tile
   >
     <v-card-title>
-      {{ message }}
+      {{ title }}
     </v-card-title>
 
     <v-card-text>
       <v-container>
         <v-row>
           <v-col>
-            <chess-game />
+            <chess-game
+              v-if="game"
+              :game="game"
+            />
           </v-col>
 
           <v-col>
             <v-data-table
               :headers="headers"
-              :items="moves"
+              :items="revisions"
               hide-default-footer
-            />
+            >
+              <template v-slot:item="{ index, item }">
+                <tr>
+                  <td>{{ index + 1 }}</td>
+                  <td>{{ item.w.move }}</td>
+                  <td>{{ item.b.move }}</td>
+                </tr>
+              </template>
+            </v-data-table>
           </v-col>
         </v-row>
       </v-container>
@@ -29,6 +40,10 @@
 
 <script>
   import { gql } from "@apollo/client"
+
+  import { drop, find, map, splitEvery } from "ramda"
+
+  import { WHITE, BLACK } from "~/share/constants/chess"
 
   import ChessGame from "./ChessGame"
 
@@ -41,9 +56,7 @@
 
     data() {
       return {
-        message: "",
-
-        moves: [ ],
+        game: null,
 
         headers: [
           { text: "#", sortable: false },
@@ -53,20 +66,83 @@
       }
     },
 
+    computed: {
+      whitePlayer() {
+        if (!this.game) {
+          return
+        }
+
+        return find(player => player.color === WHITE, this.game.players)
+      },
+
+      blackPlayer() {
+        if (!this.game) {
+          return
+        }
+
+        return find(player => player.color === BLACK, this.game.players)
+      },
+
+      title() {
+        if (!this.game) {
+          return ""
+        }
+
+        const whitePlayer = this.whitePlayer
+        const blackPlayer = this.blackPlayer
+
+        return [
+          whitePlayer.user.displayName,
+          `(${whitePlayer.user.rating})`,
+          " vs ",
+          blackPlayer.user.displayName,
+          `(${blackPlayer.user.rating})`
+        ].join(" ")
+      },
+
+      revisions() {
+        if (!this.game) {
+          return []
+        }
+
+        return map(([white, black]) => {
+          return {
+            [WHITE]: white,
+            [BLACK]: black
+          }
+        }, splitEvery(2, drop(1, this.game.revisions)))
+      }
+    },
+
     created() {
-      this.fetchMessage()
+      this.getGame(this.$route.params.uuid)
     },
 
     methods: {
-      fetchMessage() {
+      getGame(uuid) {
         const query = gql`
           {
-            hello
+            getGame(uuid: "${uuid}") {
+              players {
+                color
+                user {
+                  uuid
+                  displayName
+                  rating
+                }
+              }
+
+              revisions {
+                type
+                fen
+                move
+              }
+            }
           }
         `
 
         this.$store.state.query({ query })
-          .then(({ data }) => this.message = data )
+          .then(({ data }) => this.game = data.getGame )
       }
     }
   }
