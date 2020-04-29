383fb78480fbe2586264c7e6be2d3687f5bf21fb
Sonny Michaud <michaud.sonny@gmail.com>
2020-04-29 11:56:39 -0500

    test authentication store

--- test/client/store/authentication.test.js
+++ test/client/store/authentication.test.js
@@ -1,30 +1,92 @@
 import test from "ava"
 
-import { identity } from "ramda"
+import { stub} from "sinon"
 
-import store from "~/client/store/authentication"
+import { v4 } from "uuid"
+import { clone } from "ramda"
 
-test("login", t => {
+import Vue from "vue"
+import Vuex from "vuex"
+
+Vue.use(Vuex)
+
+import authentication from "~/client/store/authentication"
+
+test("loggedIn getter: false when user is not set", t => {
+  const store = new Vuex.Store(clone(authentication))
+
+  t.false(store.getters["loggedIn"])
+})
+
+test("loggedIn getter: true when user is set", t => {
+  const store = new Vuex.Store(clone(authentication))
+  store.commit("login", { })
+
+  t.true(store.getters["loggedIn"])
+})
+
+test("loggedOut getter: true when user is not set", t => {
+  const store = new Vuex.Store(clone(authentication))
+
+  t.true(store.getters["loggedOut"])
+})
+
+test("loggedOut getter: false when user is set", t => {
+  const store = new Vuex.Store(clone(authentication))
+  store.commit("login", { })
+
+  t.false(store.getters["loggedOut"])
+})
+
+test("displayName getter", t => {
+  const displayName = v4()
+
+  const store = new Vuex.Store(clone(authentication))
+  t.is("Bughouse", store.getters["displayName"])
+
+  store.commit("login", { displayName })
+  t.is(displayName, store.getters["displayName"])
+})
+
+test("uuid getter", t => {
+  const uuid = v4()
+
+  const store = new Vuex.Store(clone(authentication))
+  t.is(null, store.getters["uuid"])
+
+  store.commit("login", { uuid })
+  t.is(uuid, store.getters["uuid"])
+})
+
+test("login mutation: stores the user object", t => {
+  const store = clone(authentication)
+
   const state = { user: null }
-
-  const user = { }
+  const user = v4()
 
   store.mutations.login(state, user)
 
   t.is(user, state.user)
 })
 
-test("logout", t => {
+test("logout mutation: unsets user", t => {
+  const store = clone(authentication)
   const state = { user: { } }
+
   store.mutations.logout(state)
-  t.falsy(state.user)
+
+  t.is(null, state.user)
 })
 
-test("actions: logout", async t => {
-  const commit = identity
-  const rootState = { fetch: identity }
+test("logout action", async t => {
+  const store = clone(authentication)
 
-  store.actions.logout({ commit, rootState })
+  const commit = stub()
 
-  t.pass()
+  const fetch = () => ({ status: 205 })
+  const rootState = { fetch }
+
+  await store.actions.logout({ commit, rootState })
+
+  t.true(commit.calledThrice)
 })
